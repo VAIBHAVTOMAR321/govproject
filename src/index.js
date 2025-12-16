0751fae188a3a359d2a5491d9a41b9f3adc0e007
VAIBHAVTOMAR321 <95899330+VAIBHAVTOMAR321@users.noreply.github.com>
2025-12-16 11:03:43 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"; // IMPORTANT: This import is required here
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* This is the ONE and ONLY Router in your app */}
-    <BrowserRouter>
+    <BrowserRouter basename="/horticulture">
       <App />
     </BrowserRouter>
   </React.StrictMode>
