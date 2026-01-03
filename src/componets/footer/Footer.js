0f85462aff82108ee581bb0f5446a307e8ad3af6
VAIBHAVTOMAR321 <95899330+VAIBHAVTOMAR321@users.noreply.github.com>
2026-01-03 12:30:07 +0530

    changes

--- src/componets/footer/Footer.js
+++ src/componets/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="copyright-footer">
-      <p>© 2025 Horticulture. All Rights Reserved | Design by &nbsp;Brainrock</p>
+      <p>© 2026 DHO Kotdwar. All Rights Reserved | Design by &nbsp;Brainrock</p>
     </footer>
   );
 }
