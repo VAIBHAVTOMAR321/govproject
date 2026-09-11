25c57aed28f5466e98243ea9830ab2d0913d2434
Deepika Chauhan <deepikachauhan8988@gmail.com>
2026-09-11 10:44:14 +0530

    updated

--- src/componets/footer/Footer.js
+++ src/componets/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="copyright-footer">
+    <footer className="copyright-footer footer-positioned">
       <p>© 2026 DHO Kotdwar. All Rights Reserved | Design by &nbsp;Brainrock</p>
     </footer>
   );
